99a8c27fba1ab812765458d2e0143114f10e1cf0
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-11-22 14:35:09 +0500

    first commit

---
+++ hrmanager/src/main/java/uz/hrmanager/hrmanager/HrmanagerApplication.java
@@ -0,0 +1,13 @@
+package uz.hrmanager.hrmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmanagerApplication.class, args);
+	}
+
+}
